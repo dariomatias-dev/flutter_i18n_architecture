0da70cbc05da63ed058dee3650e0e2abb713f14a
dariomatias-dev <matiasdario75@gmail.com>
2026-02-02 15:17:00 -0300

    feat(lib/src/core): created `BuildContext` extension
    - Added a method to retrieve translations via `LocalizedMessage` linked to `BuildContext`.

---
+++ lib/src/core/extensions/build_context_extension.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:flutter_localization/l10n/app_localizations.dart';
+
+import 'package:flutter_localization/l10n/l10n_messages.dart';
+
+extension BuildContextExtension on BuildContext {
+  String l10n(LocalizedMessage message) {
+    final l10n = AppLocalizations.of(this);
+
+    if (l10n == null) {
+      throw FlutterError('AppLocalizations not found in context');
+    }
+
+    return message(l10n);
+  }
+}
